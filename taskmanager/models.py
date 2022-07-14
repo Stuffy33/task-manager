22e2e2cc73add739498276bfd8bd913788898f72
Stuffy33 <eric96an@gmail.com>
2022-07-14 13:46:30 +0000

    task get/post

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,14 +1,16 @@
 from taskmanager import db
 
+
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.string(25), unique=True, nullable=False)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        #repr__repr__to represent itself in the form of a string
+        # __repr__ to represent itself in the form of a string
         return self.category_name
+
 
 class Task(db.Model):
     # schema for the Task model
@@ -20,8 +22,7 @@ class Task(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-    #repr__repr__to represent itself in the form of a string
-        #return f"#{self.id} - Task:{self.task_name} | Urgent:{self.is_urgent}"
+        # __repr__ to represent itself in the form of a string
         return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //side nav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
